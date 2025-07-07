2e1ab8ba163b796563430f3b046840fa35c8e697
Samarth Saxena <163576924+samarth10000@users.noreply.github.com>
2025-07-07 18:15:47 +0530

    Updated StarPattern.js logic

--- Day4/StarPattern.js
+++ Day4/StarPattern.js
@@ -6,14 +6,14 @@
 // *****
 
 // So the i here is responsible for rows and j is responsible for coloums
-let n = 5;
-for (let i = 0; i < 6; i++) {
-  let row = "";
-  for (let j = 0; j < n; j++) {
-    row = row + "*";
-  }
-  console.log(row);
-}
+// let n = 5;
+// for (let i = 0; i < 6; i++) {
+//   let row = "";
+//   for (let j = 0; j < n; j++) {
+//     row = row + "*";
+//   }
+//   console.log(row);
+// }
 
 // Que - 2
 // *
@@ -22,15 +22,15 @@ for (let i = 0; i < 6; i++) {
 // ****
 // *****
 
-let n = 5;
+// let n = 5;
 
-for (let i = 0; i < n; i++) {
-  let row = "";
-  for (let j = 0; j < i + 1; j++) {
-    row = row + "*";
-  }
-  console.log(row);
-}
+// for (let i = 0; i < n; i++) {
+//   let row = "";
+//   for (let j = 0; j < i + 1; j++) {
+//     row = row + "*";
+//   }
+//   console.log(row);
+// }
 
 // Que - 3
 // 1
@@ -40,15 +40,15 @@ for (let i = 0; i < n; i++) {
 // 12345
 
 // Ans -
-let n = 5;
+// let n = 5;
 
-for (let i = 0; i < n; i++) {
-  let row = "";
-  for (let j = 1; j <= i + 1; j++) {
-    row = row + j;
-  }
-  console.log(row);
-}
+// for (let i = 0; i < n; i++) {
+//   let row = "";
+//   for (let j = 1; j <= i + 1; j++) {
+//     row = row + j;
+//   }
+//   console.log(row);
+// }
 
 // Que - 4
 // 1
@@ -58,12 +58,114 @@ for (let i = 0; i < n; i++) {
 // 55555
 
 // Ans -
-let n = 5;
+// let n = 5;
 
-for (let i = 1; i <= n; i++) {
-  let row = "";
-  for (let j = 0; j < i; j++) {
-    row = row + i;
-  }
-  console.log(row);
-}
+// for (let i = 1; i <= n; i++) {
+//   let row = "";
+//   for (let j = 0; j < i; j++) {
+//     row = row + i;
+//   }
+//   console.log(row);
+// }
+
+// Que -5
+// 12345
+// 1234
+// 123
+// 12
+// 1
+
+// Ans -
+// let n = 5;
+// for (let i = 0; i < n; i++) {
+//   let row = "";
+//   for (let j = 0; j < n - i; j++) {
+//     row = row + (j + 1);
+//   }
+//   console.log(row);
+// }
+
+// Que - 6
+
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+// let n = 5;
+
+// for (let i = 0; i < n; i++) {
+//   let row = "";
+//   for (let j = 0; j < n - i; j++) {
+//     row = row + "*";
+//   }
+//   console.log(row);
+// }
+
+//Que - 7
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+// let n = 5;
+
+// for (i = 0; i < n; i++) {
+//   let row = "";
+//   for (let j = 0; j < n - (i + 1); j++) {
+//     row = row + " ";
+//   }
+//   for (let k = 0; k < i + 1; k++) {
+//     row = row + "*";
+//   }
+//   console.log(row);
+// }
+
+// Que - 8
+
+// 1
+// 10
+// 101
+// 1010
+// 10101
+
+// let n = 5;
+// for (let i = 0; i < n; i++) {
+//   let row = "";
+//   let toggle = 1;
+//   for (let j = 0; j < i + 1; j++) {
+//     row = row + toggle;
+//     if (toggle == 1) {
+//       toggle = 0;
+//     } else {
+//       toggle = 1;
+//     }
+//   }
+//   console.log(row);
+// }
+
+// Que - 8
+
+// 1
+// 10
+// 101
+// 1010
+// 10101
+
+// let n = 5;
+// let toggle = 1;
+// for (let i = 0; i < n; i++) {
+//   let row = "";
+
+//   for (let j = 0; j < i + 1; j++) {
+//     row = row + toggle;
+//     if (toggle == 1) {
+//       toggle = 0;
+//     } else {
+//       toggle = 1;
+//     }
+//   }
+//   console.log(row);
+// }
