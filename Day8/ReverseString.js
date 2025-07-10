2d6520748482d72fc4418818a09977154abf016f
Samarth Saxena <163576924+samarth10000@users.noreply.github.com>
2025-07-10 22:18:46 +0530

    Day8

---
+++ Day8/ReverseString.js
@@ -0,0 +1,10 @@
+var reverseString = function (s) {
+  let len = s.length;
+  let halfLen = Math.floor(len / 2);
+
+  for (let i = 0; i < halfLen; i++) {
+    let temp = s[i];
+    s[i] = s[len - i - 1];
+    s[len - i - 1] = temp;
+  }
+};
