4ece40c3534ae9b4859e00aaabaf40f008c28f27
Samarth Saxena <163576924+samarth10000@users.noreply.github.com>
2025-07-11 23:58:59 +0530

    Day9

---
+++ Day9/BestTimetoBuyStocks.js
@@ -0,0 +1,11 @@
+var maxProfit = function (prices) {
+  let maxProfit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      if (prices[j] - prices[i] > maxProfit) {
+        maxProfit = prices[j] - prices[i];
+      }
+    }
+  }
+  return maxProfit;
+};
