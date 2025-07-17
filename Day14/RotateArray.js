ba6ed10334fb74b310d0b010de1710787571a2ea
Samarth Saxena <163576924+samarth10000@users.noreply.github.com>
2025-07-18 02:29:41 +0530

    day14

---
+++ Day14/RotateArray.js
@@ -0,0 +1,13 @@
+var rotate = function (nums, k) {
+  const n = nums.length;
+  k = k % n;
+  const rotated = new Array(n).fill(0);
+
+  for (let i = 0; i < n; i++) {
+    rotated[(i + k) % n] = nums[i];
+  }
+
+  for (let i = 0; i < n; i++) {
+    nums[i] = rotated[i];
+  }
+};
