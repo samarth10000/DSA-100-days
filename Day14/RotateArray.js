f1d5dbfb5fdef818f0f12eeff3acc6e1de6608ef
Samarth Saxena <163576924+samarth10000@users.noreply.github.com>
2025-07-25 17:19:41 +0530

    Add Some More Solution

--- Day14/RotateArray.js
+++ Day14/RotateArray.js
@@ -1,4 +1,4 @@
-// BruteForce
+// BruteForce for Right shift
 var rotate = function (nums, k) {
   let n = nums.length;
   let arr = [];
@@ -12,7 +12,7 @@ var rotate = function (nums, k) {
   return nums;
 };
 
-// optimal
+optimal;
 var rotate = function (nums, k) {
   const n = nums.length;
   k = k % n;
@@ -26,3 +26,35 @@ var rotate = function (nums, k) {
     nums[i] = rotated[i];
   }
 };
+
+// Rotate array to the left by d places
+
+// BruteForce Approach------------------------------------------
+let arr = [3, 4, 5, 6, 7, 8, 9];
+const LeftRotate = (arr) => {
+  let n = arr.length;
+  let d = 3;
+  d = d % n;
+  let temp = [];
+  for (let i = 0; i < d; i++) {
+    temp[i] = arr[i];
+  }
+  for (let i = d; i < n; i++) {
+    arr[i - d] = arr[i];
+  }
+  for (let i = 0; i < d; i++) {
+    arr[n - d + i] = temp[i];
+  }
+
+  return arr;
+};
+
+// console.log(LeftRotate(arr));
+
+// Optimal Approach with out using any extra space----------------
+
+// let arr = [3, 4, 5, 6, 7, 8, 9];
+
+//reverse first d elements
+// reverse rest of the elements by d places
+// reverse the whole array by d element
