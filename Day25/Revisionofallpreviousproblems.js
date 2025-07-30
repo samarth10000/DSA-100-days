f93ba8adbbbf6ef1f655a5bb7a8937d07e2b651c
Samarth Saxena <163576924+samarth10000@users.noreply.github.com>
2025-07-30 23:27:51 +0530

    practice

---
+++ Day25/Revisionofallpreviousproblems.js
@@ -0,0 +1,58 @@
+// Questions List
+// Largest, Second Largest
+
+// Patterns
+
+// Remove Duplicates from a Sorted Array
+
+// Remove Element
+
+// Reverse String
+
+// Best Time to Buy and Sell Stock
+
+// Merge Sorted Array
+
+// Move Zeros
+
+// Max Consecutive Ones
+
+// Missing Number
+
+// Two Sum
+
+// Rotate Array
+
+// Left Rotate by D Places
+
+// Right Rotate by D Places
+
+// Recursion
+
+// Sum of First N Numbers
+
+// Sum of All Elements in an Array
+
+// Factorial
+
+// Power of Two
+
+// Sum of All Odd
+
+// Fibonacci Series in Recursion
+
+// Binary Search
+
+// Bubble Sort
+
+// Selection Sort
+
+// Insertion Sort
+
+// Merge Sort (Divide & Conquer)
+
+// Majority Element
+
+// Sort 0, 1 and 2 (Dutch National Flag Algorithm)
+
+// Maximum Subarray
