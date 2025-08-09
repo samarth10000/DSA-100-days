ccbbd5095be07da0aa1b0b91c33367072e05bd87
Samarth Saxena <163576924+samarth10000@users.noreply.github.com>
2025-08-10 04:05:58 +0530

    day32

---
+++ day32/reverselinkedlist.js
@@ -0,0 +1,25 @@
+// Problem Statement:
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+// Examples:
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+// Input: head = [1,2]
+// Output: [2,1]
+// Input: head = []
+// Output: []
+// Constraints:
+// The number of nodes in the list is in the range [0, 5000].
+// -5000 ≤ Node.val ≤ 5000
+
+var reverseList = function (head) {
+  let prev = null;
+  let curr = head;
+  while (curr) {
+    let temp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = temp;
+  }
+  return prev;
+};
